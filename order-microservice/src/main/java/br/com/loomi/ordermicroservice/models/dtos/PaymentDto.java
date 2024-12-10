479de3844b3eb53639b54085b35576ff655fda6b
c-henrique-dev <carlossoaressantana@hotmail.com>
2024-12-09 23:40:39 -0300

    feat: create Dtos

---
+++ order-microservice/src/main/java/br/com/loomi/ordermicroservice/models/dtos/PaymentDto.java
@@ -0,0 +1,23 @@
+package br.com.loomi.ordermicroservice.models.dtos;
+
+import br.com.loomi.ordermicroservice.models.enums.PaymentStatus;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class PaymentDto {
+    private String id;
+    private UUID orderId;
+    private UUID customerId;
+    private BigDecimal amount;
+    private PaymentStatus status;
+    private UUID transactionId;
+}
