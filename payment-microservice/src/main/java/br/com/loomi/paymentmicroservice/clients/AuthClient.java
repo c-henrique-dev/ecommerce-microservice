555bdf32ef78656e39da386b95a880aacfb143a9
c-henrique-dev <carlossoaressantana@hotmail.com>
2024-12-09 23:50:46 -0300

    feat: create AuthClient

---
+++ payment-microservice/src/main/java/br/com/loomi/paymentmicroservice/clients/AuthClient.java
@@ -0,0 +1,17 @@
+package br.com.loomi.paymentmicroservice.clients;
+
+import br.com.loomi.paymentmicroservice.security.CustomUserDetails;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name = "auth-microservice", path = "/auth")
+public interface AuthClient {
+
+    @GetMapping(path = "/{email}")
+    ResponseEntity<CustomUserDetails> loadByUsername(@PathVariable("email") String email);
+
+    @GetMapping(path = "/valid/{token}")
+    ResponseEntity<String> validToken(@PathVariable("token") String token);
+}
