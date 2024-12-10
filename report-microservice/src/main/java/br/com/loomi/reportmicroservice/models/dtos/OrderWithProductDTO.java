3e3104cc2e7a274b7c2846de91134ee935423d4f
c-henrique-dev <carlossoaressantana@hotmail.com>
2024-12-09 23:46:32 -0300

    feat: create OrderWithProductsDto

---
+++ report-microservice/src/main/java/br/com/loomi/reportmicroservice/models/dtos/OrderWithProductDTO.java
@@ -0,0 +1,17 @@
+package br.com.loomi.reportmicroservice.models.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+public class OrderWithProductDTO {
+    private UUID productId;
+    private String productName;
+    private BigDecimal productPrice;
+    private Integer totalQtd;
+    private BigDecimal totalValor;
+}
