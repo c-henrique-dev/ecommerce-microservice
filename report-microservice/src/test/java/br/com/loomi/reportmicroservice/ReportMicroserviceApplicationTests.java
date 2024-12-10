09714c17d14e4cb64e58e4033381df58e291dc93
c-henrique-dev <carlossoaressantana@hotmail.com>
2024-12-09 23:47:52 -0300

    feat: create test file

---
+++ report-microservice/src/test/java/br/com/loomi/reportmicroservice/ReportMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.loomi.reportmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReportMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
