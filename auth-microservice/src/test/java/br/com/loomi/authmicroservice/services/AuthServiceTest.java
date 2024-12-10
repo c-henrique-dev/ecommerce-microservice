8b160b0dc4eb0ce04852b1e595beef4c78b5fde4
c-henrique-dev <carlossoaressantana@hotmail.com>
2024-12-09 23:22:16 -0300

    feat: create unit tests

---
+++ auth-microservice/src/test/java/br/com/loomi/authmicroservice/services/AuthServiceTest.java
@@ -0,0 +1,35 @@
+package br.com.loomi.authmicroservice.services;
+
+import br.com.loomi.authmicroservice.clients.CustomerClient;
+import br.com.loomi.authmicroservice.enums.UserType;
+import br.com.loomi.authmicroservice.models.Customer;
+import br.com.loomi.authmicroservice.models.User;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.ResponseEntity;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class AuthServiceTest {
+    @InjectMocks
+    private AuthService  authService;
+
+    @Mock
+    private CustomerClient customerClient;
+
+    @Test
+    public void createPlanet_WithValidData_ReturnsPlanet() {
+        User user = User.builder().email("carlossoaressantana@hotmail.com").type(UserType.CUSTOMER).name("teste").build();
+        Customer customer = Customer.builder().fullName("Carlos Henrique").contact("81996589021").address("dsfdgsdgsdf").status(false).user(user).build();
+        when(customerClient.loadByEmail("carlossoaressantana@hotmail.com")).thenReturn(ResponseEntity.ok(customer));
+
+        Customer sut = authService.loadUserByEmail(user.getEmail());
+
+        assertThat(sut).isEqualTo(customer);
+    }
+}
---
+++ auth-microservice/src/test/java/br/com/loomi/authmicroservice/services/JwtServiceTest.java
@@ -0,0 +1,54 @@
+package br.com.loomi.authmicroservice.services;
+
+import br.com.loomi.authmicroservice.enums.UserType;
+import br.com.loomi.authmicroservice.models.Customer;
+import br.com.loomi.authmicroservice.models.User;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(MockitoExtension.class)
+public class JwtServiceTest {
+
+    @InjectMocks
+    private JwtService jwtService;
+
+    private String expiration;
+    private String keySignature;
+
+    @BeforeEach
+    public void setUp() {
+        expiration = "60";
+        keySignature = "test-key";
+
+        jwtService = new JwtService();
+        jwtService.expiration = expiration;
+        jwtService.keySignature = keySignature;
+    }
+
+    @Test
+    public void createToken_WithValidData_ReturnsToken() {
+        User user = User.builder()
+                .email("carlossoaressantana@hotmail.com")
+                .type(UserType.CUSTOMER)
+                .name("teste")
+                .build();
+
+        Customer customer = Customer.builder()
+                .fullName("Carlos Henrique")
+                .contact("81996589021")
+                .address("dsfdgsdgsdf")
+                .status(false)
+                .user(user)
+                .build();
+
+        String token = jwtService.getToken(customer);
+
+        assertThat(token).isInstanceOf(String.class);
+        assertThat(token).isNotEmpty();
+    }
+}
