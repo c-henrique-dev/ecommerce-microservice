ab7dfbeb8e331fc6672b634b05b6f2a48968aba4
c-henrique-dev <carlossoaressantana@hotmail.com>
2024-12-09 23:41:53 -0300

    feat: create OrderRepository

---
+++ order-microservice/src/main/java/br/com/loomi/ordermicroservice/repositories/OrderRepository.java
@@ -0,0 +1,24 @@
+package br.com.loomi.ordermicroservice.repositories;
+
+import br.com.loomi.ordermicroservice.models.entities.Order;
+import feign.Param;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+public interface OrderRepository extends JpaRepository<Order, UUID> {
+    @Query("SELECT oi.productId, SUM(oi.qtd) as totalQtd, SUM(oi.qtd * oi.pricePerUnit) as totalValor " +
+            "FROM Order o " +
+            "JOIN o.orderItems oi " +
+            "WHERE o.createdAt BETWEEN :initialDate AND :endDate " +
+            "GROUP BY oi.productId")
+    List<Object[]> findOrdersByPeriod(@Param("initialDate") LocalDateTime initialDate,
+                                      @Param("endDate") LocalDateTime endDate);
+
+
+
+}
