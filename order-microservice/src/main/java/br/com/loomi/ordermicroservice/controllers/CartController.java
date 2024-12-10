878e3fc2a54b0ae3f231b92971a7240a1823bb71
c-henrique-dev <carlossoaressantana@hotmail.com>
2024-12-09 23:40:08 -0300

    feat: create controllers

---
+++ order-microservice/src/main/java/br/com/loomi/ordermicroservice/controllers/CartController.java
@@ -0,0 +1,71 @@
+package br.com.loomi.ordermicroservice.controllers;
+
+import br.com.loomi.ordermicroservice.models.entities.Cart;
+import br.com.loomi.ordermicroservice.services.CartService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("order/cart")
+@Tag(name = "Cart", description = "Endpoints related to shopping cart management")
+public class CartController {
+
+    private final CartService cartService;
+
+    public CartController(CartService cartService) {
+        this.cartService = cartService;
+    }
+
+    @Operation(
+            summary = "Retrieve the shopping cart of a customer",
+            description = "Returns the shopping cart associated with the provided customer ID"
+    )
+    @GetMapping("/{customerId}")
+    public ResponseEntity<Cart> getCart(
+            @Parameter(description = "Customer ID to retrieve the shopping cart", required = true)
+            @PathVariable UUID customerId) {
+
+        Cart cart = cartService.getCart(customerId);
+        return ResponseEntity.ok(cart);
+    }
+
+    @Operation(
+            summary = "Add a product to the shopping cart",
+            description = "Adds a specified product to the customer's shopping cart with the given quantity"
+    )
+    @PostMapping("{customerId}")
+    public ResponseEntity<Cart> addToCart(
+            @Parameter(description = "Customer ID to add the product to the cart", required = true)
+            @PathVariable UUID customerId,
+
+            @Parameter(description = "Product ID to add to the shopping cart", required = true)
+            @RequestParam UUID productId,
+
+            @Parameter(description = "Quantity of the product to add", required = true)
+            @RequestParam Integer quantity) {
+
+        ResponseEntity<Cart> updatedCart = cartService.addToCart(customerId, productId, quantity);
+        return ResponseEntity.ok(updatedCart.getBody());
+    }
+
+    @Operation(
+            summary = "Remove a product from the shopping cart",
+            description = "Removes a specified product from the customer's shopping cart"
+    )
+    @DeleteMapping("{customerId}")
+    public ResponseEntity<Cart> removeFromCart(
+            @Parameter(description = "Customer ID from which the product will be removed", required = true)
+            @PathVariable UUID customerId,
+
+            @Parameter(description = "Product ID to remove from the shopping cart", required = true)
+            @RequestParam UUID productId) {
+
+        Cart updatedCart = cartService.removeFromCart(customerId, productId);
+        return ResponseEntity.ok(updatedCart);
+    }
+}
---
+++ order-microservice/src/main/java/br/com/loomi/ordermicroservice/controllers/PaymentController.java
@@ -0,0 +1,35 @@
+package br.com.loomi.ordermicroservice.controllers;
+
+import br.com.loomi.ordermicroservice.models.dtos.PaymentDto;
+import br.com.loomi.ordermicroservice.services.PaymentService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("order/payment")
+@Tag(name = "Payment", description = "Endpoints related to simulating payments for an order")
+public class PaymentController {
+
+    private final PaymentService paymentService;
+
+    public PaymentController(PaymentService paymentService) {
+        this.paymentService = paymentService;
+    }
+
+    @Operation(
+            summary = "Simulate a payment for an order",
+            description = "This endpoint simulates payment for an order. If successful, debits stock and updates the order status to RECEIVED; otherwise, updates the status to PREPARATION."
+    )
+    @PostMapping("{orderId}")
+    public ResponseEntity<Map> simulatePayment(
+            @PathVariable @Parameter(description = "Order identifier for payment") UUID orderId) throws JsonProcessingException {
+        return paymentService.simulatePayment(orderId);
+    }
+}
