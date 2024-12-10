d7524360c1533a0509e69343364e82ce438580a8
c-henrique-dev <carlossoaressantana@hotmail.com>
2024-12-09 23:24:26 -0300

    feat: create initial setup

---
+++ customer-microservice/src/test/java/br/com/loomi/customermicroservice/CustomerMicroserviceSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.loomi.customermicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerMicroserviceSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
