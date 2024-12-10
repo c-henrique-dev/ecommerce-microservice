6711eb5b27f2b7e37020eee3bb222fa4b400eced
c-henrique-dev <carlossoaressantana@hotmail.com>
2024-12-09 23:52:08 -0300

    feat: create payment entity

---
+++ payment-microservice/src/main/java/br/com/loomi/paymentmicroservice/models/Payment.java
@@ -0,0 +1,56 @@
+package br.com.loomi.paymentmicroservice.models;
+
+import br.com.loomi.paymentmicroservice.models.enums.PaymentStatus;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.Random;
+import java.util.UUID;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Payment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "order_id", nullable = false)
+    private UUID orderId;
+
+    @Column(name = "customer_id", nullable = false)
+    private UUID customerId;
+
+    @Column(nullable = false)
+    private BigDecimal amount;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private PaymentStatus status;
+
+    @Column(name = "transaction_id")
+    private UUID transactionId;
+
+    @CreationTimestamp
+    @Column(name = "created_At", updatable = false, nullable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_At", nullable = false)
+    private LocalDateTime updatedAt;
+
+    public void setStatus() {
+        PaymentStatus[] statuses = PaymentStatus.values();
+        int randomIndex = new Random().nextInt(statuses.length);
+        this.status = statuses[randomIndex];
+    }
+}
