77e83ec764ffeacc10eb540d631156946ae6d262
c-henrique-dev <carlossoaressantana@hotmail.com>
2024-12-09 23:34:05 -0300

    feat: create Product entity

---
+++ product-microservice/src/main/java/br/com/loomi/productmicroservice/models/entities/Product.java
@@ -0,0 +1,50 @@
+package br.com.loomi.productmicroservice.models.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    private BigDecimal price;
+
+    @Column(nullable = false)
+    private Integer qtd;
+
+    @CreationTimestamp
+    @Column(updatable = false, nullable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
+    public Product(String name, BigDecimal price, Integer qtd) {
+        this.name = name;
+        this.price = price;
+        this.qtd = qtd;
+    }
+}
