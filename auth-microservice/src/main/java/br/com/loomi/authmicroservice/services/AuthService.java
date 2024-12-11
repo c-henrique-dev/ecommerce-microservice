2a1ba8663582cdf34f61313087554da268591d3a
c-henrique-dev <carlossoaressantana@hotmail.com>
2024-12-11 09:31:12 -0300

    feat: add redis to cache login

--- auth-microservice/src/main/java/br/com/loomi/authmicroservice/services/AuthService.java
+++ auth-microservice/src/main/java/br/com/loomi/authmicroservice/services/AuthService.java
@@ -10,23 +10,31 @@ import br.com.loomi.authmicroservice.models.dtos.TokenDto;
 import br.com.loomi.authmicroservice.models.dtos.UserDto;
 import br.com.loomi.authmicroservice.security.CustomUserDetailsService;
 import feign.FeignException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.time.Duration;
+
 @Service
+@Slf4j
 public class AuthService {
 
     private CustomerClient customerClient;
     private CustomUserDetailsService customUserDetailsService;
     private JwtService jwtService;
+    private final RedisTemplate<String, String> redisTemplate;
 
-    public AuthService(CustomerClient customerClient, CustomUserDetailsService customUserDetailsService, JwtService jwtService) {
+    public AuthService(CustomerClient customerClient, CustomUserDetailsService customUserDetailsService,
+                       JwtService jwtService, RedisTemplate<String, String> redisTemplate) {
         this.customerClient = customerClient;
         this.customUserDetailsService = customUserDetailsService;
         this.jwtService = jwtService;
+        this.redisTemplate = redisTemplate;
     }
 
     public Customer loadUserByEmail(String email) {
@@ -53,14 +61,25 @@ public class AuthService {
                     .email(authDto.getEmail())
                     .password(authDto.getPassword()).build();
             BeanUtils.copyProperties(user, userDto);
+
             Customer customer = this.customerClient.loadByEmail(authDto.getEmail()).getBody();
 
-            if (customer.getStatus() == false) {
-                throw new BadRequestException("e-mail not validated");
+            if (Boolean.FALSE.equals(customer.getStatus())) {
+                throw new BadRequestException("E-mail not validated");
             }
 
             this.customUserDetailsService.auth(userDto);
+
+            String cachedToken = redisTemplate.opsForValue().get(authDto.getEmail());
+            if (cachedToken != null) {
+                log.info("getting token from redis");
+                return ResponseEntity.ok(new TokenDto(cachedToken));
+            }
+
             String token = jwtService.getToken(customer);
+
+            redisTemplate.opsForValue().set(authDto.getEmail(), token, Duration.ofHours(1));
+
             return ResponseEntity.ok(new TokenDto(token));
         } catch (FeignException e) {
             String content = e.contentUTF8();
