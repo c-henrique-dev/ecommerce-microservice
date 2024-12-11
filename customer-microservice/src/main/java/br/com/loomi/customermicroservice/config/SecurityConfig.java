458797c40a9d97360c209e53a16fedc3a56594d0
c-henrique-dev <carlossoaressantana@hotmail.com>
2024-12-11 09:34:29 -0300

    feat: allow public access to /actuator/** endpoints

--- customer-microservice/src/main/java/br/com/loomi/customermicroservice/config/SecurityConfig.java
+++ customer-microservice/src/main/java/br/com/loomi/customermicroservice/config/SecurityConfig.java
@@ -35,7 +35,8 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.POST, "/customer").permitAll()
                         .requestMatchers(HttpMethod.GET, "/customer/{email}").permitAll()
                         .requestMatchers(HttpMethod.GET, "/customer/get/{email}").permitAll()
-                        .requestMatchers(HttpMethod.GET, "/customer/confirm").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/customer/mail/confirm").permitAll()
+                        .requestMatchers("/actuator/**").permitAll()
                         .anyRequest().authenticated())
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
