851cd5bafa1c8ad95c02c1b613d8451bca86fdba
c-henrique-dev <carlossoaressantana@hotmail.com>
2024-12-11 09:41:44 -0300

    feat: create configuration class to enable Async

---
+++ customer-microservice/src/main/java/br/com/loomi/customermicroservice/config/AsyncConfig.java
@@ -0,0 +1,9 @@
+package br.com.loomi.customermicroservice.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@Configuration
+@EnableAsync
+public class AsyncConfig {
+}
