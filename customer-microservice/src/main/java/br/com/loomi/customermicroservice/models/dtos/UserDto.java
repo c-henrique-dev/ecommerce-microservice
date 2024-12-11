a9bcb8e52fb132435d7c53169d6011183ffca98f
c-henrique-dev <carlossoaressantana@hotmail.com>
2024-12-11 09:40:48 -0300

    refactor: remove user name

--- customer-microservice/src/main/java/br/com/loomi/customermicroservice/models/dtos/UserDto.java
+++ customer-microservice/src/main/java/br/com/loomi/customermicroservice/models/dtos/UserDto.java
@@ -12,10 +12,6 @@ import lombok.NoArgsConstructor;
 @Data
 @Builder
 public class UserDto {
-    @NotBlank(message = "Name is required.")
-    @Size(max = 50, message = "Name must not exceed 50 characters.")
-    private String name;
-
     @NotBlank(message = "Email is required.")
     @Email(message = "Email must be valid.")
     private String email;
--- customer-microservice/src/main/java/br/com/loomi/customermicroservice/models/entities/User.java
+++ customer-microservice/src/main/java/br/com/loomi/customermicroservice/models/entities/User.java
@@ -23,9 +23,6 @@ public class User {
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
-    @Column(nullable = false)
-    private String name;
-
     @Column(nullable = false, unique = true)
     private String email;
 
